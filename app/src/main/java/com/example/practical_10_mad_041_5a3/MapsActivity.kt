389d5830e0c1817fea5a0edcf3f8e98445b31f19
pratik-2004 <kotharirajkumar1967@gmail.com>
2023-10-31 00:05:46 +0530

    PRACTICAL_10

---
+++ app/src/main/java/com/example/practical_10_mad_041_5a3/MapsActivity.kt
@@ -0,0 +1,46 @@
+package com.example.practical_10_mad_041_5a3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.practical_10_mad_041_5a3.databinding.ActivityMapsBinding
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private var lat:Double = 0.0
+    private var log:Double = 0.0
+    private var title = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val obj = intent.getSerializableExtra("Object") as Person
+        //Log.i(TAG,"onCreate:Object:$obj")
+        lat = obj.latitude
+        log = obj.longitude
+        title = obj.name
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(lat,  log )
+        mMap.addMarker(MarkerOptions().position(sydney).title(title))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
